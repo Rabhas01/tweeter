8e73293dbfbaa96f6249a78f06901f6eef733f38
Rabhas <rabhas.vashisht@gmail.com>
2022-03-31 23:06:22 +0000

    Implementing the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#tweet-text").on("input", function () {
+    const charLeft = 140 - this.value.length;
+    const counter = $(this).closest(".submitForm").find(".counter");
+
+    counter.text(`${charLeft}`);
+    counter.toggleClass("extraChrt", charLeft < 0);
+  });
+});
