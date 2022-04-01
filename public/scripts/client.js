9654fc9224b82457709b1a88e518df158793e73f
Rabhas <rabhas.vashisht@gmail.com>
2022-04-01 04:26:01 +0000

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,31 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 $(document).ready(function () {
 
   $( ".submitForm" ).submit(function( event ) {
-    alert( "Handler for .submit() called." );
     let query = $( this ).serialize();
     event.preventDefault();
     $.ajax({
@@ -35,6 +34,18 @@ $(document).ready(function () {
       data: query 
     })
   });
+
+  const loadtweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "/tweets/",
+      dataType: 'JSON'
+    }).then(function( data ) {
+      renderTweets(data)
+    });
+  }
+
+loadtweets()
 
 const createTweetElement = function(tweetData) {
   const time = timeago.format(tweetData.created_at)
@@ -72,6 +83,6 @@ const renderTweets = function(tweets) {
   }
 }
 
-renderTweets(data);
+// renderTweets(data);
 
 });
