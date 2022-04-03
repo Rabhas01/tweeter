b2278fa49956da72e1f807807f3d76e80399aa07
Rabhas <rabhas.vashisht@gmail.com>
2022-04-03 06:35:29 +0000

    Adding final tweeks to the tweeter project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,118 +1,91 @@
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-$(document).ready(function () {
+$(document).ready(function() {
   //hidden error messages
   $('#empty').hide();
   $('#exceed').hide();
 
-    $( ".submitForm" ).submit(function( event ) {
-    let query = $( this ).serialize();
-    let tweetLength = $('#tweet-text').val().length
+  $(".submitForm").submit(function(event) {
+    let query = $(this).serialize();
+    let tweetLength = $('#tweet-text').val().length;
     if (tweetLength === 0) {
-      event.preventDefault()
+      event.preventDefault();
       $('#exceed').hide();
       $('#empty').slideDown(500);
     } else if (tweetLength > 140) {
-      event.preventDefault()
+      event.preventDefault();
       $('#empty').hide();
       $('#exceed').slideDown(500);
     } else {
    
-    event.preventDefault();
-    $.ajax({
-      method: "POST",
-      url: "/tweets/",
-      data: query 
-    }).then(function(response) {
+      event.preventDefault();
+      $.ajax({
+        method: "POST",
+        url: "/tweets/",
+        data: query
+      }).then(function(response) {
       // console.log(response)
-      $('#tweet-text').val('');
-      $('.tweet-container').empty();
-      loadTweets(response);
-    });
-}
-});
+        $('#tweet-text').val('');
+        $('.counter').val(140);
+        $('.tweet-container').empty();
+        loadTweets(response);
+      });
+    }
+  });
 
-// escapes the input text to avoid cross-site scripting attacks -> used in createTweetElement
-const escape = function (str) {
+  // escapes the input text to avoid cross-site scripting attacks -> used in createTweetElement
+  const escape = function(str) {
   //creates a new element to append the text to
-  let div = document.createElement("div");
-  // appends the text to the element using createTextNode to escape unsafe characters
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+    let div = document.createElement("div");
+    // appends the text to the element using createTextNode to escape unsafe characters
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-const createTweetElement = function(tweetData) {
-  const time = timeago.format(tweetData.created_at)
-  const tweet = $(`
-  <article class="tweet">
-     <header> 
-      <span class="name"> 
-      <img src='${escape(tweetData.user.avatars)}'>
-        ${escape(tweetData.user.name)}
-      </span>
-      <p class="tag">${escape(tweetData.user.handle)}</p>
-    </header>
-    <p class="text">${escape(tweetData.content.text)}</p>
-    <footer> 
-      <span class="days-ago">
-        ${escape(time)}
-      </span>
-      <span class="icon">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i> 
-      </span>
-    </footer>
-  </article>`);
-  return tweet;
-}
+  const createTweetElement = function(tweetData) {
+    const time = timeago.format(tweetData.created_at);
+    const tweet = $(`
+      <article class="tweet">
+        <header> 
+          <span class="name"> 
+          <img src='${escape(tweetData.user.avatars)}'>
+            ${escape(tweetData.user.name)}
+          </span>
+          <p class="tag">${escape(tweetData.user.handle)}</p>
+        </header>
+        <p class="text">${escape(tweetData.content.text)}</p>
+        <footer> 
+          <span class="days-ago">
+            ${escape(time)}
+          </span>
+          <span class="icon">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i> 
+          </span>
+        </footer>
+      </article>`);
+    return tweet;
+  };
 
-const renderTweets = function(tweets) {
+  const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  for(let tweetData of tweets) { 
-    const $tweet = createTweetElement(tweetData);
-    $('.tweet-container').append($tweet); 
-  }
-}
+    for (let tweetData of tweets) {
+      const $tweet = createTweetElement(tweetData);
+      $('.tweet-container').append($tweet);
+    }
+  };
 
-// renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets/",
+      dataType: 'JSON'
+    }).then(function(data) {
+      renderTweets(data.reverse());
+    });
+  };
 
-const loadTweets = function () {
-  $.ajax({
-    method: "GET",
-    url: "/tweets/",
-    dataType: 'JSON'
-  }).then(function(data) {
-    renderTweets(data)
-  });
-}
-
-loadTweets()
+  loadTweets();
 
 });
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,7 @@
-$(document).ready(function () {
-  $("#tweet-text").on("input", function () {
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
     const charLeft = 140 - this.value.length;
     const counter = $(this).closest(".submitForm").find(".counter");
-
     counter.text(`${charLeft}`);
     counter.toggleClass("extraChrt", charLeft < 0);
   });
